0d246d9750e684e1b6aa5a0a9aa8267a8a586250
simonpojok <simonpojok@gmail.com>
2022-11-19 17:13:10 +0300

    implemented password validators

--- CoffeeBeenAndroid/src/main/java/org/mdeveloperz/coffeebeen/android/components/text_input.kt
+++ CoffeeBeenAndroid/src/main/java/org/mdeveloperz/coffeebeen/android/components/text_input.kt
@@ -1,6 +1,7 @@
 package org.mdeveloperz.coffeebeen.android.components
 
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -9,9 +10,15 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Visibility
+import androidx.compose.material.icons.rounded.VisibilityOff
 import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
@@ -19,11 +26,12 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import org.mdeveloperz.coffeebeen.android.gray
 
+
 data class TextInputFieldValue(
     val data: TextFieldValue,
     val errorMessage: String,
     val placeHolder: String,
-    val label: String
+    val label: String,
 )
 
 @OptIn(ExperimentalMaterialApi::class)
@@ -62,7 +70,7 @@ fun TextInputField(
                         enabled = true,
                         interactionSource = remember { MutableInteractionSource() },
                         singleLine = true,
-                        visualTransformation = VisualTransformation.None
+                        visualTransformation = VisualTransformation.None,
                     )
                 }
             )
@@ -71,10 +79,74 @@ fun TextInputField(
             text = value.errorMessage,
             color = MaterialTheme.colors.error,
             fontSize = 13.sp,
-            modifier = Modifier.padding(top = 4.dp)
+            modifier = Modifier
+                .padding(top = 4.dp)
                 .align(Alignment.End)
         )
     }
+}
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun PasswordInputField(
+    value: TextInputFieldValue,
+    onTextValueChanged: (TextFieldValue) -> Unit
+) {
+    var passwordVisible by rememberSaveable { mutableStateOf(false) }
+
+    Column(modifier = Modifier.fillMaxWidth()) {
+        Text(text = value.label, color = gray, fontSize = 16.sp)
+        Surface(
+            modifier = Modifier
+                .padding(top = 12.dp)
+                .border(
+                    width = 1.dp, color = gray, shape = RoundedCornerShape(
+                        topStartPercent = 30,
+                        bottomStartPercent = 30,
+                        topEndPercent = 30,
+                        bottomEndPercent = 30
+
+                    )
+                )
+        ) {
+            TextField(
+                value.data,
+                onValueChange = onTextValueChanged,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(50.dp),
+                colors = TextFieldDefaults.textFieldColors(
+                    textColor = Color.Gray,
+                    disabledTextColor = Color.Transparent,
+                    backgroundColor = Color.White,
+                    focusedIndicatorColor = Color.Transparent,
+                    unfocusedIndicatorColor = Color.Transparent,
+                    disabledIndicatorColor = Color.Transparent
+                ),
+                placeholder = { Text(text = value.placeHolder) },
+                visualTransformation = if (passwordVisible) VisualTransformation.None else PasswordVisualTransformation('*'),
+                trailingIcon = {
+                    val image = if (!passwordVisible)
+                        Icons.Rounded.Visibility
+                    else Icons.Rounded.VisibilityOff
+                    val description =
+                        if (passwordVisible) "Hide password" else "Show password"
+                    IconButton(onClick = { passwordVisible = !passwordVisible }) {
+                        Icon(imageVector = image, description)
+                    }
+                }
+            )
+        }
+        Text(
+            text = value.errorMessage,
+            color = MaterialTheme.colors.error,
+            fontSize = 13.sp,
+            modifier = Modifier
+                .padding(top = 4.dp)
+                .align(Alignment.End)
+        )
+    }
+
 }
 
 @Preview
---
+++ CoffeeBeenAndroid/src/main/java/org/mdeveloperz/coffeebeen/android/screens/registration/user_password_screen.kt
@@ -0,0 +1,97 @@
+package org.mdeveloperz.coffeebeen.android.screens.registration
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import org.mdeveloperz.coffeebeen.android.components.*
+import org.mdeveloperz.coffeebeen.android.green
+import org.mdeveloperz.coffeebeen.android.screens.registration.widgets.RegistrationHeader
+
+@Composable
+fun UserPasswordScreen() {
+    var password by remember {
+        mutableStateOf(
+            TextInputFieldValue(
+                data = TextFieldValue(""),
+                errorMessage = "",
+                label = "Password",
+                placeHolder = "*****"
+            )
+        )
+    }
+
+    var confirmPassword by remember {
+        mutableStateOf(
+            TextInputFieldValue(
+                data = TextFieldValue(""),
+                errorMessage = "",
+                label = "Confirm Password",
+                placeHolder = "*****"
+            )
+        )
+    }
+
+    fun handleSubmitUserNames() {
+    }
+
+    Scaffold(
+        topBar = { TopToolbar() {} }
+    ) { contentPadding ->
+        Column(
+            modifier = Modifier
+                .padding(contentPadding)
+                .padding(top = 20.dp, start = 12.dp, end = 12.dp)
+        ) {
+            RegistrationHeader()
+
+            PasswordInputField(
+                value = password
+            ) {
+                password = password.copy(
+                    data = it
+                )
+            }
+
+            Spacer(modifier = Modifier.height(15.dp))
+
+            PasswordInputField(
+                value = confirmPassword
+            ) {
+                confirmPassword = confirmPassword.copy(
+                    data = it
+                )
+            }
+
+            Spacer(modifier = Modifier.weight(weight = 5F, fill = true))
+
+            Button(
+                onClick = ::handleSubmitUserNames,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(50.dp),
+                colors = ButtonDefaults.buttonColors(
+                    backgroundColor = green,
+                    contentColor = Color.White
+                )
+            ) {
+                Text(text = "Create Account")
+            }
+
+            Spacer(modifier = Modifier.weight(weight = 1F, fill = true))
+        }
+    }
+}
+
+@Preview
+@Composable
+fun UserPasswordScreenPreview() {
+    UserPasswordScreen()
+}
