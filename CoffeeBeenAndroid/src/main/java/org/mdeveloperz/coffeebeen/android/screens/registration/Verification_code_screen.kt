8ec0800b080afc44c331e9b84f292513be8d4219
simonpojok <simonpojok@gmail.com>
2022-11-19 17:21:56 +0300

    added the opt screen

---
+++ CoffeeBeenAndroid/src/main/java/org/mdeveloperz/coffeebeen/android/screens/registration/Verification_code_screen.kt
@@ -0,0 +1,32 @@
+package org.mdeveloperz.coffeebeen.android.screens.registration
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import org.mdeveloperz.coffeebeen.android.components.TopToolbar
+import org.mdeveloperz.coffeebeen.android.screens.registration.widgets.RegistrationHeader
+
+@Composable
+fun VerificationCodeScreen() {
+    Scaffold(
+        topBar = { TopToolbar() {} }
+    ) { contentPadding ->
+        Column(
+            modifier = Modifier
+                .padding(contentPadding)
+                .padding(top = 20.dp, start = 12.dp, end = 12.dp)
+        ) {
+            RegistrationHeader(message = "Input OTP Verification")
+        }
+    }
+}
+
+@Preview
+@Composable
+fun VerificationCodeScreenPreview() {
+    VerificationCodeScreen()
+}
